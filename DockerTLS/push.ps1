45874d7cf8f75d753ee74a7b00a4156de145cbf7
Maksym Kursin <maxim@kursin.pp.ua>
2019-07-31 16:57:27 +0300

    fix tags

--- DockerTLS/push.ps1
+++ DockerTLS/push.ps1
@@ -1,5 +1,6 @@
 $version=$(select-string -Path Dockerfile -Pattern "ENV VERSION").ToString().split()[-1]
-docker tag dockertls sergiimatusepam/dockertls-windows:$version
-docker tag dockertls sergiimatusepam/dockertls-windows
+docker tag sergiimatusepam/dockertls-windows sergiimatusepam/dockertls-windows:$version
+docker tag sergiimatusepam/dockertls-windows sergiimatusepam/dockertls-windows:dind
 docker push sergiimatusepam/dockertls-windows:$version
+docker push sergiimatusepam/dockertls-windows:dind
 docker push sergiimatusepam/dockertls-windows
